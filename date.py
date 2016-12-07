15e15321396db44cbd7d1a53de0d659086ff8792
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2016-12-07 10:29:25 -0800

    see the comments and improve the program

--- date.py
+++ date.py
@@ -8,3 +8,6 @@ date = int(d[0:n[0]])
 month = int(d[n[0]+1:n[1]])
 year = int(d[n[1]+1:len(d)+1])
 print date, "th day of", month,"th month in the year", year
+
+#dont store all the positions of the separating characters it is a waste of memory
+# only store the previous one and update it each time
